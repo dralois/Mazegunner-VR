ddbe3873c8dd2e7434e502e3d3bfd11e586b2cdd
Michael Schlicker <schlicker.michael91@gmail.com>
2018-06-16 01:13:03 +0200

    Player v0.1 added

---
+++ SGJ Chill Edition/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int playerNumber = 1;
+
+    [Header("Movement Settings")]
+    public Vector3 movementSpeed = new Vector3(5, 5, 5);
+
+    public string xMovementInputString = "L_XAxis_";
+    public string yMovementInputString = "L_YAxis_";
+    public string zMovementInputString = "";
+
+    public bool invertMovementX = false;
+    public bool invertMovementY = true;
+    public bool invertMovementZ = false;
+
+    [Header("Rotation Settings")]
+    public Vector3 rotationSpeed = new Vector3(5, 5, 5);
+
+    public string xRotationInputString = "";
+    public string yRotationInputString = "";
+    public string zRotationInputString = "R_YAxis_";
+
+    public bool inverRotationX = false;
+    public bool invertRotationY = false;
+    public bool invertRotationZ = false;
+
+    [Header("Misc Settings")]
+    public bool borderCheck;
+    public Vector3 border1;
+    public Vector3 border2;
+
+    CharacterController controller;
+    // Use this for initialization
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerControl();
+        PassiveMovement();
+    }
+
+    private void PassiveMovement()
+    {
+
+
+        //BorderChecks
+        if (borderCheck)
+            transform.position = Util.SetInBetween(transform.position, border1, border2);
+    }
+
+    private void PlayerControl()
+    {
+        Move(transform.TransformDirection(new Vector3(
+       Util.GetInputAxisSafe(xMovementInputString) * movementSpeed.x * (invertMovementX ? -1 : 1),
+       Util.GetInputAxisSafe(yMovementInputString) * movementSpeed.y * (invertMovementY ? -1 : 1),
+       Util.GetInputAxisSafe(zMovementInputString) * movementSpeed.z * (invertMovementZ ? -1 : 1))));
+
+        gameObject.transform.Rotate(new Vector3(
+       Util.GetInputAxisSafe(xRotationInputString) * rotationSpeed.x * (inverRotationX ? -1 : 1),
+       Util.GetInputAxisSafe(yRotationInputString) * rotationSpeed.y * (invertRotationY ? -1 : 1),
+       Util.GetInputAxisSafe(zRotationInputString) * rotationSpeed.z * (invertRotationZ ? -1 : 1)));
+
+    }
+
+    private void Move(Vector3 direction)
+    {
+            print(controller.Move(direction * Time.deltaTime));
+    }
+
+
+}
---
+++ SGJ Chill Edition/Assets/Scripts/Util.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Util
+{
+
+    #region Random Util
+
+    
+    public static int RandomInt(int a, int b, List<int> exceptions = null)
+    {
+
+        return Random.Range(a, b + 1);
+    }
+
+    public static Vector3 RandomVector3(Vector3 a, Vector3 b, float distance, List<Vector3> exceptions = null)
+    {
+        int safetyCounter = 2;
+        int counter = 5;
+
+        Vector3 res = RandomVector3(a, b);
+        bool isOk = false;
+        while (safetyCounter > 0 && !isOk)
+        {
+            counter--;
+            if (counter <= 0)
+            {
+                distance /= 2;
+                counter = 5;
+                safetyCounter--;
+            }
+
+            isOk = true;
+            if (exceptions != null && exceptions.Count != 0)
+            {
+                foreach (Vector3 pos in exceptions)
+                {
+                    if ((pos - res).magnitude <= distance)
+                    {
+                        res = RandomVector3(a, b);
+                        isOk = false;
+                        break;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+    public static Vector3 RandomVector3(Vector3 a, Vector3 b)
+    {
+        float x = Random.Range(a.x, b.x);
+        float y = Random.Range(a.y, b.y);
+        float z = Random.Range(a.z, b.z);
+        return new Vector3(x, y, z);
+    }
+    #endregion
+
+    public static float Slerp(float a, float b, float t)
+    {
+        Vector3 va = new Vector3(a, 0, 0);
+        Vector3 vb = new Vector3(b, 0, 0);
+        return Vector3.Slerp(va, vb, t).x;
+
+    }
+    #region region Checks
+    public static bool InBetween(float value, float a, float b)
+    {
+        if (a < b)
+        {
+            return value >= a && value <= b;
+        }
+        else
+        {
+            return value <= a && value >= b;
+        }
+    }
+
+    public static float SetInBetween(float value, float a, float b)
+    {
+
+        if (a > b)
+        {
+            if (a <= value)
+            {
+                return a;
+            }
+            if (b >= value)
+            {
+                return b;
+            }
+        }
+        else
+        {
+            if (b <= value)
+            {
+                return b;
+            }
+            if (a >= value)
+            {
+                return a;
+            }
+        }
+        return value;
+    }
+
+    public static bool InBetween(Vector3 value, Vector3 a, Vector3 b)
+    {
+        bool isX = InBetween(value.x, a.x, b.x);
+        bool isY = InBetween(value.y, a.y, b.y);
+        bool isZ = InBetween(value.z, a.z, b.z);
+        return isX && isY && isZ;
+    }
+
+    public static Vector3 SetInBetween(Vector3 value, Vector3 a, Vector3 b)
+    {
+        float x = SetInBetween(value.x, a.x, b.x);
+        float y = SetInBetween(value.y, a.y, b.y);
+        float z = SetInBetween(value.z, a.z, b.z);
+        return new Vector3(x, y, z);
+    }
+
+    #endregion
+
+    #region Input
+    public static float GetInputAxisSafe(string inputString, int playerNumber)
+    {
+        if (inputString.Length == 0)
+            return 0;
+        return Input.GetAxis(inputString + playerNumber);
+    }
+
+    public static float GetInputAxisSafe(string inputString)
+    {
+        if (inputString.Length == 0)
+            return 0;
+        return Input.GetAxis(inputString);
+    }
+    #endregion
+}
