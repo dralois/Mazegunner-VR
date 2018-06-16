f1c39bc9e066f4176be7a72f145ff54d88505a0f
Alexander Theimer <x.taxel.x@gmail.com>
2018-06-16 18:12:28 +0200

    Added railgun sounds

--- SGJ Chill Edition/Assets/Scripts/Railgun.cs
+++ SGJ Chill Edition/Assets/Scripts/Railgun.cs
@@ -4,22 +4,43 @@ using UnityEngine;
 
 public class Railgun : MonoBehaviour {
 
-	public bool isShooting = false;
-	public bool isActive = false;
-	Animator animator;
+	public bool isShooting {
+		get {return _shooting;}
+		set {
+			animator.SetBool("shooting", value);
+			_shooting = value;
+		}
+	}
 
+	public bool isActive {
+		get {return _active;}
+		set {
+			if(value && !_active &&  railgun_activate != null){
+				audio.PlayOneShot(railgun_activate);
+			}
+			animator.SetBool("activated", value);
+			_active = value;
+		}
+	}
+
+	public AudioClip railgun_activate;
+	Animator animator;
+	AudioSource audio;
+
+	private bool _shooting = false;
+	private bool _active = false;
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator>();
+		audio = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		animator.SetBool("shooting", isShooting);
-		animator.SetBool("activated", isActive);
 	}
 
 	public void FireLaser () {
-		Debug.Log("Imma firin' mah lazor");
+		//Debug.Log("Imma firin' mah lazor");
+		audio.Play();
 	}
 }
