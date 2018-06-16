f0e71204bf8efc70a5b90f6e851133c4989d823c
Michael Schlicker <schlicker.michael91@gmail.com>
2018-06-16 09:59:34 +0200

    player animations

--- SGJ Chill Edition/Assets/Scripts/PlayerMovement.cs
+++ SGJ Chill Edition/Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 
-public class PlayerMovement : NetworkBehaviour {
+public class PlayerMovement : NetworkBehaviour
+{
     public int playerNumber = 1;
 
     [Header("Movement Settings")]
@@ -36,43 +37,70 @@ public class PlayerMovement : NetworkBehaviour {
     public Vector3 border2;
 
     CharacterController controller;
+    Animator animator;
     // Use this for initialization
-    void Start() {
+    void Start()
+    {
         controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update() {
-        if (!isLocalPlayer) {
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
             return;
         }
 
         PlayerControl();
         PassiveMovement();
+        print(animator.GetBool("walking"));
     }
 
-    private void PassiveMovement() {
+    private void PassiveMovement()
+    {
         //BorderChecks
         if (borderCheck)
             transform.position = Util.SetInBetween(transform.position, border1, border2);
-    }
-
-    private void PlayerControl() {
-       Move(transform.TransformDirection(new Vector3(
-       Util.GetInputAxisSafe(xMovementInputString) * movementSpeed.x * (invertMovementX ? -1 : 1),
-       Util.GetInputAxisSafe(yMovementInputString) * movementSpeed.y * (invertMovementY ? -1 : 1),
-       Util.GetInputAxisSafe(zMovementInputString) * movementSpeed.z * (invertMovementZ ? -1 : 1))));
-
-       gameObject.transform.Rotate(new Vector3(
-       Util.GetInputAxisSafe(xRotationInputString) * rotationSpeed.x * (inverRotationX ? -1 : 1),
-       Util.GetInputAxisSafe(yRotationInputString) * rotationSpeed.y * (invertRotationY ? -1 : 1),
-       Util.GetInputAxisSafe(zRotationInputString) * rotationSpeed.z * (invertRotationZ ? -1 : 1)));
 
     }
 
-    private void Move(Vector3 direction) {
-        print(controller.Move(direction * Time.deltaTime));
+    private void PlayerControl()
+    {
+        //Movement
+        Vector3 movedir = new Vector3(
+        Util.GetInputAxisSafe(xMovementInputString) * movementSpeed.x * (invertMovementX ? -1 : 1),
+        0,
+        Util.GetInputAxisSafe(zMovementInputString) * movementSpeed.z * (invertMovementZ ? -1 : 1));
+
+        if (Mathf.Abs(movedir.magnitude) > 0)
+        {
+            animator.SetBool("running", true);
+
+            animator.SetBool("running backwards", movedir.z < 0);
+
+        }
+        else
+        {
+            animator.SetBool("running backwards", false);
+            animator.SetBool("running", false);
+        }
+
+        Move(transform.TransformDirection(movedir));
+
+        //Rotation
+        gameObject.transform.Rotate(new Vector3(
+        Util.GetInputAxisSafe(xRotationInputString) * rotationSpeed.x * (inverRotationX ? -1 : 1),
+        Util.GetInputAxisSafe(yRotationInputString) * rotationSpeed.y * (invertRotationY ? -1 : 1),
+        Util.GetInputAxisSafe(zRotationInputString) * rotationSpeed.z * (invertRotationZ ? -1 : 1)));
+
+        //Jumping
+
     }
 
-
+    private void Move(Vector3 direction)
+    {
+        controller.Move(direction * Time.deltaTime);
+    }
 }
