30da88e3efc9251276d39230bfaa55e006392925
Michael Schlicker <schlicker.michael91@gmail.com>
2018-06-16 13:14:33 +0200

    animations improved

---
+++ SGJ Chill Edition/Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour
+{
+
+    //[SerializeField]
+    public Behaviour[] componentsToDisable;
+
+    void Start()
+    {
+        // Disable components that should only be
+        // active on the player that we control
+        if (!isLocalPlayer)
+        {
+            DisableComponents();
+        }
+    }
+
+    void DisableComponents()
+    {
+        for (int i = 0; i < componentsToDisable.Length; i++)
+        {
+            componentsToDisable[i].enabled = false;
+        }
+    }
+}
