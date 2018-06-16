d0ed34ee1a58853b20d1afa6ad011c213bb6f89a
Alexander Theimer <x.taxel.x@gmail.com>
2018-06-16 16:32:47 +0200

    Added Railgun Script and Animation

---
+++ SGJ Chill Edition/Assets/Scripts/Railgun.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Railgun : MonoBehaviour {
+
+	public bool isShooting = false;
+	public bool isActive = false;
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		animator.SetBool("shooting", isShooting);
+		animator.SetBool("activated", isActive);
+	}
+
+	public void FireLaser () {
+		Debug.Log("Imma firin' mah lazor");
+	}
+}
