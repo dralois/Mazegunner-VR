a53826e68f61e3ec6de52fcc985cd3301da732fb
Manuel Geiger <manuel.geiger@tum.de>
2018-06-16 13:15:26 +0200

    implemented a circle select (touchpad)

---
+++ SGJ Chill Edition/Assets/Scripts/CircleSelect.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CircleSelect : MonoBehaviour, IScrollHandler {
+
+    [Tooltip("How many options the user can select.")]
+    public int options;
+
+    public void OnScroll(PointerEventData eventData)
+    {
+        float a = Vector2.SignedAngle(Vector2.up, eventData.scrollDelta) + 180;
+        float step = 361.0f / options;
+        int id = Mathf.FloorToInt(a / step);
+        onSelect(id);
+    }
+
+    // This does not require a click
+    public void onSelect(int id)
+    {
+
+    }
+}
