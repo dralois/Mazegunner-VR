7c46835ea8fce5745251d15005614176ea00df6e
Konstantin Kraus <konstantin_kraus@t-online.de>
2018-06-16 15:53:26 +0200

    Added interface for pickups

---
+++ SGJ Chill Edition/Assets/Scripts/Pickups/PlayerStats.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerStats : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SpeedBoost(float amount, float time) {
+
+    }
+
+    public void Invisibility(float time) {
+
+    }
+
+    public void Shield(float time) {
+
+    }
+}
